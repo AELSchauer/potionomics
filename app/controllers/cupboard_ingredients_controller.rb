211c258ee4672a64537345454a118e4ff9013860
Ashley Schauer <ashley.schauer@givecampus.com>
2023-08-03 22:30:18 -0400

    add update

--- app/controllers/cupboard_ingredients_controller.rb
+++ app/controllers/cupboard_ingredients_controller.rb
@@ -4,4 +4,18 @@ class CupboardIngredientsController < ApplicationController
   def index
     @cupboard_ingredients = @cupboard.cupboard_ingredients.where(discovered: true).joins(:ingredient)
   end
+
+  def edit
+    @cupboard_ingredient = @cupboard.cupboard_ingredients.find(params[:id])
+  end
+
+  def update
+    @cupboard_ingredient = @cupboard.cupboard_ingredients.find(params[:id])
+
+    if @cupboard_ingredient.update(quantity: params[:quantity])
+      redirect_to cupboard_cupboard_ingredients_path(@cupboard)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
 end
