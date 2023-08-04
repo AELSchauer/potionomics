c65a4870b18cef4110fed3c4e1e6996cc5a266f1
Ashley Schauer <ashley.schauer@givecampus.com>
2023-08-03 20:21:39 -0400

    cupboard ingredient model updates

--- app/decorators/cupboard_ingredient_decorator.rb
+++ app/decorators/cupboard_ingredient_decorator.rb
@@ -2,13 +2,13 @@ class CupboardIngredientDecorator < ApplicationDecorator
   decorates :cupboard_ingredient
   include Draper::LazyHelpers
 
-  def display_quinn_tab
-    return nil if object.quinn_location.nil?
-    (object.quinn_location+10)/10
-  end
-
   def display_quinn_slot
     return nil if object.quinn_location.nil?
     (object.quinn_location % 10)+1
+  end
+
+  def display_quinn_tab
+    return nil if object.quinn_location.nil?
+    (object.quinn_location+10)/10
   end
 end
--- app/models/cupboard_ingredient.rb
+++ app/models/cupboard_ingredient.rb
@@ -2,10 +2,12 @@ class CupboardIngredient < ApplicationRecord
   belongs_to :cupboard
   belongs_to :ingredient
 
-  validates :cupboard, uniqueness: { scope: :ingredient }
-  validates :quantity, numericality: { greater_than: -1 }
+  validates :ingredient, uniqueness: { scope: :cupboard, message: "already exists in this cupboard" }
+  validates :quantity, presence: true, numericality: { greater_than: -1 }
 
-  after_save do
-    cupboard.update(updated_at: self.updated_at)
+  delegate  :display_quinn_slot, :display_quinn_tab, to: :decorator
+
+  def decorator
+    CupboardIngredientDecorator.new(self)
   end
 end
