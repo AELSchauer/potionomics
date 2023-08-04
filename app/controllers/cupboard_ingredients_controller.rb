7347e42a7886e4aec30766ce04c654f1871782dc
Ashley Schauer <ashley.schauer@givecampus.com>
2023-08-04 00:25:43 -0400

    add/edit cupboard_ingredients

--- app/controllers/cupboard_ingredients_controller.rb
+++ app/controllers/cupboard_ingredients_controller.rb
@@ -5,6 +5,27 @@ class CupboardIngredientsController < ApplicationController
     @cupboard_ingredients = @cupboard.cupboard_ingredients.where(discovered: true).joins(:ingredient)
   end
 
+  def new
+    @ingredients = Ingredient.joins(:cupboard_ingredients).where(cupboard_ingredients: { cupboard: @cupboard, discovered: false })
+    @cupboard_ingredient = @cupboard.cupboard_ingredients.first
+  end
+
+  def create
+    @cupboard_ingredient = @cupboard.cupboard_ingredients.find_by(ingredient_id: params[:ingredient_id])
+
+    @cupboard_ingredient.assign_attributes(quantity: params[:quantity], discovered: true)
+    if params[:quinn_stock]
+      @cupboard_ingredient.assign_attributes(quinn_location: @cupboard.cupboard_ingredients.pluck(:quinn_location).compact.max + 1)
+    end
+    
+    if @cupboard_ingredient.save
+      redirect_to cupboard_cupboard_ingredients_path(@cupboard, params[:tab] ? { tab: params[:tab] } : {})
+    else
+      @ingredients = Ingredient.joins(:cupboard_ingredients).where(cupboard_ingredients: { cupboard: @cupboard, discovered: false })
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def edit
     @cupboard_ingredient = @cupboard.cupboard_ingredients.find(params[:id])
   end
